d78a1ac01a7bfe582216656f43525ca40d632f5f
Jannis R <mail@jannisr.de>
2016-10-24 10:49:59 +0200

    captions :sparkles:

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const axis = (opt) => (column) =>
 	h('polyline', Object.assign(opt.axisProps(column), {
 		points: points([
 			[0, 0], [
-				polarToX(column.angle, opt.size / 2),
-				polarToY(column.angle, opt.size / 2)
+				polarToX(column.angle, opt.chartSize / 2),
+				polarToY(column.angle, opt.chartSize / 2)
 			]
 		])
 	}))
@@ -40,25 +40,38 @@ const axis = (opt) => (column) =>
 const shape = (columns, opt) => (data) =>
 	h('path', Object.assign(opt.shapeProps(data), {
 		d: opt.smoothing(columns.map((col) => [
-			polarToX(col.angle, data[col.key] * opt.size / 2 * opt.maxShapeSize),
-			polarToY(col.angle, data[col.key] * opt.size / 2 * opt.maxShapeSize)
+			polarToX(col.angle, data[col.key] * opt.chartSize / 2),
+			polarToY(col.angle, data[col.key] * opt.chartSize / 2)
 		]))
 	}))
 
 const scale = (opt, value) =>
 	h('circle', Object.assign(opt.scaleProps(value), {
-		cx: 0, cy: 0, r: value * opt.size / 2 * opt.maxShapeSize
+		cx: 0, cy: 0, r: value * opt.chartSize / 2
 	}))
 
+const caption = (opt) => (col) =>
+	h('text', Object.assign(opt.captionProps(col), {
+		x: round(polarToX(col.angle, opt.size / 2 * .95)),
+		y: round(polarToY(col.angle, opt.size / 2 * .95)),
+		dy: (opt.captionProps(col).fontSize || 2) / 2
+	}), col.caption)
+
 const defaults = {
-	size: 100, // size of the whole chart
+	size: 100, // size of the chart (including captions)
 	axes: true, // show axes?
 	scales: 3, // show scale circles?
-	maxShapeSize: .9, // where on the axes is the value 1?
+	captions: true, // show captions?
+	captionsPosition: 1.2, // where on the axes are the captions?
 	smoothing: noSmoothing, // shape smoothing function
 	axisProps: () => ({className: 'axis'}),
 	scaleProps: () => ({className: 'scale', fill: 'none'}),
-	shapeProps: () => ({className: 'shape'})
+	shapeProps: () => ({className: 'shape'}),
+	captionProps: () => ({
+		className: 'caption',
+		textAnchor: 'middle', fontSize: 3,
+		fontFamily: 'sans-serif'
+	})
 }
 
 const render = (columns, data, opt = {}) => {
@@ -67,6 +80,7 @@ const render = (columns, data, opt = {}) => {
 	if (!Array.isArray(data))
 		throw new Error('data must be an array')
 	opt = Object.assign({}, defaults, opt)
+	opt.chartSize = opt.size / opt.captionsPosition
 
 	columns = Object.keys(columns)
 		.map((key, i, all) => ({
@@ -81,6 +95,7 @@ const render = (columns, data, opt = {}) => {
 	const groups = [
 		h('g', data.map(shape(columns, opt)))
 	]
+	if (opt.captions) groups.unshift(h('g', columns.map(caption(opt))))
 	if (opt.axes) groups.unshift(h('g', columns.map(axis(opt))))
 	if (opt.scales > 0) {
 		const scales = []
